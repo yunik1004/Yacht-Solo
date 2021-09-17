e8590811665ac56f1a41dd46bcceda800bbb82d8
yunik1004 <inkyuhak@gmail.com>
2021-09-17 19:49:51 +0900

    Remove margin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ onMounted(() => {
   const scene = new THREE.Scene()
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
   const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9)
+  renderer.setSize(window.innerWidth, window.innerHeight)
   document.getElementById('canvas-container')?.appendChild(renderer.domElement)
 
   const geometry = new THREE.BoxGeometry(1, 1, 1)
